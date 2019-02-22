aa7f2fc361054887c48e004a720d7e7512d9efa8
Ville Kähärä <kahara.ville@gmail.com>
2019-02-22 17:53:15 +0200

    spelling

--- kahville/src/components/ProfileCard.vue
+++ kahville/src/components/ProfileCard.vue
@@ -7,7 +7,7 @@
           <v-card-text>
             <h3 class="headline">Hello World!</h3>
             <p>Hi! My name is Ville Kähärä. I am a Bachelor of Engineering student in the field of information technology. While specializing in game programming my skills lay in problem solving. As a bonus I'm a person who loves coffee.</p>
-            <p>In my free time I like to play video games and go for a ride with a bicycle. My favourite games are the Euro Truck Simulator 2 among the few single player games like the Rise of the Thomb Raider and the Dust: An Elysian Tale </p>
+            <p>In my free time I like to play video games and go for a ride with a bicycle. My favourite games are the Euro Truck Simulator 2 among the few single player games like the Rise of the Thomb Raider and the Dust: An Elysian Tale.</p>
           </v-card-text>
         </v-container>
       </v-card-title>
