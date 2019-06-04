8d81d142b9875e41a5400166dda811ee1136788b
Ville Kähärä <kahara.ville@gmail.com>
2019-06-04 20:13:12 +0300

    deploy

---
+++ precache-manifest.3b1920d9eae25aa2926105b42b28d89a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "47e5902266e95f3abfaf",
+    "url": "/css/chunk-d752abb2.4c93734c.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "47e5902266e95f3abfaf",
+    "url": "/js/chunk-d752abb2.741454c5.js"
+  },
+  {
+    "revision": "749c0268747dce492704",
+    "url": "/js/chunk-2d0a4d6c.9e4e8e06.js"
+  },
+  {
+    "revision": "7970e653b44a49d4edb2",
+    "url": "/js/home.15baadbd.js"
+  },
+  {
+    "revision": "3d9ce723545907609b57",
+    "url": "/js/chunk-74a750fb.6092850d.js"
+  },
+  {
+    "revision": "33bd76f8ca2577613896",
+    "url": "/js/chunk-vendors.6d1e7f61.js"
+  },
+  {
+    "revision": "84977a80a56787785214",
+    "url": "/js/chunk-99460924.4ac3db0e.js"
+  },
+  {
+    "revision": "63a25cd9875e5a65c0a8",
+    "url": "/js/chunk-2d0e9b24.37490d1f.js"
+  },
+  {
+    "revision": "ed93fe917f8477071cc8",
+    "url": "/js/chunk-155b5b8b.a23d1cdc.js"
+  },
+  {
+    "revision": "55c4657ed43456734c53",
+    "url": "/js/app.7e2a9d4d.js"
+  },
+  {
+    "revision": "a67fa532c3dd3d09f181e26e170602bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de6fa9eb1d9cf62c483f1a715ac989b5",
+    "url": "/img/omakuva.de6fa9eb.jpg"
+  },
+  {
+    "revision": "7970e653b44a49d4edb2",
+    "url": "/css/home.daa8cfe1.css"
+  },
+  {
+    "revision": "33bd76f8ca2577613896",
+    "url": "/css/chunk-vendors.5c3193b8.css"
+  },
+  {
+    "revision": "84977a80a56787785214",
+    "url": "/css/chunk-99460924.e574c9d1.css"
+  },
+  {
+    "revision": "3d9ce723545907609b57",
+    "url": "/css/chunk-74a750fb.96737b18.css"
+  }
+];
